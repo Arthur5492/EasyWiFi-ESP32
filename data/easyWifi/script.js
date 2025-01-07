068345c1f2e56a94b378886f1ce71ecd960976f3
Arthur5492 <arthurofr@gmail.com>
2025-01-07 02:16:08 -0300

    SVG are now inside javascript file, dinamically added on htm

--- data/easyWifi/script.js
+++ data/easyWifi/script.js
@@ -16,6 +16,16 @@
     cancelBtn: document.getElementById('cancel-button'),
     statusMsg: document.getElementById('status-message')
   };
+
+  const icons = {
+    "lock-wifi" :`<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960"  fill="#000000"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z"/></svg>`,
+    "wifi-find" :`<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960" fill="#000000"><path d="M480-120 0-601q93-93 215.5-146T480-800q142 0 264.5 53T960-601l-56 57q-81-81-190-128.5T480-720q-103 0-195 32.5T117-597l419 420-56 57Zm384-40L761-262q-18 11-38 16.5t-43 5.5q-68 0-114-46t-46-114q0-68 46-114t114-46q68 0 114 46t46 114q0 23-5.5 43T818-319l102 103-56 56ZM680-320q34 0 57-23t23-57q0-34-23-57t-57-23q-34 0-57 23t-23 57q0 34 23 57t57 23ZM480-177Z"/></svg>`,
+    "wifi-0"    :`<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960" fill="#808080"><path d="M480-120 0-600q95-97 219.5-148.5T480-800q136 0 260.5 51.5T960-600L480-120Zm0-114 364-364q-79-60-172-91t-192-31q-99 0-192 31t-172 91l364 364Z"/></svg>`,
+    "wifi-1"    :`<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960" fill="#808080"><path d="M480-120 0-600q96-98 220-149t260-51q137 0 261 51t219 149L480-120ZM361-353q25-18 55.5-28t63.5-10q33 0 63.5 10t55.5 28l245-245q-78-59-170.5-90.5T480-720q-101 0-193.5 31.5T116-598l245 245Z"/></svg>`,
+    "wifi-2"    :`<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960" fill="#808080"><path d="M480-120 0-600q96-98 220-149t260-51q137 0 261 51t219 149L480-120ZM299-415q38-28 84-43.5t97-15.5q51 0 97 15.5t84 43.5l183-183q-78-59-170.5-90.5T480-720q-101 0-193.5 31.5T116-598l183 183Z"/></svg>`,
+    "wifi-3"    :`<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960" fill="#808080"><path d="M480-120 0-600q96-98 220-149t260-51q137 0 261 51t219 149L480-120ZM232-482q53-38 116-59.5T480-563q69 0 132 21.5T728-482l116-116q-78-59-170.5-90.5T480-720q-101 0-193.5 31.5T116-598l116 116Z"/></svg>`,
+    "wifi-4"    :`<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 -960 960 960" fill="#808080"><path d="M480-120 0-600q95-97 219.5-148.5T480-800q136 0 260.5 51.5T960-600L480-120Z"/></svg>`,
+  };
   /*====================================================================
     CORE FUNCTIONS
     ====================================================================*/
@@ -174,14 +184,21 @@
   function createNetworkElement(network) {
     const div = document.createElement('div');
     div.className = 'wifi-item';
+
+    const signalNumber = getSignalIcon(network.rssi);
+    const signalIcon = icons[`wifi-${signalNumber}`];
+    const lockIcon   = network.isProtected ? icons["lock-wifi"] : '';
+
+    console.log(signalIcon);
+
     div.innerHTML = `
       <div class="wifi-icon">
-        <img src="easyWifi/icons/wifi-${getSignalIcon(network.rssi)}.svg" alt="Signal" width="24" height="24">
+        ${signalIcon}
       </div>
       <div class="wifi-info">
         <div class="wifi-name">
           ${network.ssid}
-          ${network.isProtected ? '<img src="easyWifi/icons/lock-wifi.svg" alt="Locked" width="16" height="16">' : ''}
+          ${lockIcon}
         </div>
         <div class="wifi-signal">${network.rssi}dBm</div>
       </div>
@@ -279,6 +296,16 @@
     GENERAL EVENT LISTENERS (LOAD EVERYTHING)
     ====================================================================*/
   
+  document.addEventListener("DOMContentLoaded", () => {
+    const scanButton = document.getElementById('scan-button');
+    scanButton.innerHTML = icons["wifi-find"];
+
+    const favIcon = document.getElementById('favicon');
+    favIcon.href = "data:image/svg+xml," + encodeURIComponent(icons["wifi-find"]);
+
+    
+  });
+
   // Trigger scanning on "Scan" button click
   elements.scanBtn.addEventListener('click', scanNetworks);
   
